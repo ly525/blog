c6266c0756cbfddfc0b44da3e140c84042f25e5e
小小鲁班 <lantern.done@gmail.com>
2022-08-20 22:04:27 +0800

    Update init.sh

--- sop/node/init.sh
+++ sop/node/init.sh
@@ -89,11 +89,52 @@ install_node_by_nvm() {
 install_global_npm_modules() {
   echo '[全局模块] nrm yarn 开始安装'
   npm set registry https://registry.npmmirror.com
-  npm install -g yarn nrm
+  npm install -g yarn nrm git-open
   nrm ls
-  echo '✅ [全局模块] nrm yarn 安装/配置完成'
+  echo '✅ [全局模块] nrm yarn git-open 安装/配置完成'
+  tips+="✅ [全局模块] git open 安装完成：更多阅读：https://github.com/paulirish/git-open"
   tips+="✅ [全局模块] yarn 安装完成：yarn -v $(yarn -v)"
   tips+="✅ [全局模块] nrm  安装完成：nrm --version $(nrm --version)"
+}
+
+install_autojump() {
+    if [ -f /opt/homebrew/etc/profile.d/autojump.sh ]; then
+        return
+    fi
+    # if (command_exists "j") then
+    #     return
+    # fi
+    brew install autojump
+    echo '✅ [autojump] 安装完成'
+    tips+="✅ [autojump] 安装完成 https://banbanpeppa.github.io/2019/09/16/linux/autojump/"
+
+}
+
+yes_or_no() {
+  # echo "$(type $1)"
+  printf "是否安装 [$1]，y：是，n：否 (y/n)? "
+  read answer
+  if [ "$answer" != "${answer#[Yy]}" ] ;then # this grammar (the #[] operator) means that the variable $answer where any Y or y in 1st position will be dropped if they exist.
+    echo Yes
+  else
+      echo No
+  fi
+  [ "$answer" != "${answer#[Yy]}" ]
+}
+
+install_with_homebrew() {
+    yes_or_no "autojump"     &&  brew install autojump
+    yes_or_no "postman"      &&  brew install --cask postman
+    yes_or_no "chrome"       &&  brew install --cask google-chrome
+    yes_or_no "vscode"       &&  brew install --cask visual-studio-code
+    yes_or_no "sublime"      &&  brew install --cask sublime-text
+    yes_or_no "语雀"          &&  brew install --cask yuque
+    yes_or_no "xmind"        &&  brew install --cask xmind
+    yes_or_no "腾讯会议"      &&   brew install --cask tencent-meeting
+    yes_or_no "网易云音乐"     &&  brew install --cask neteasemusic
+    yes_or_no "folx"         &&  brew install --cask folx
+    yes_or_no "微信开发者工具" &&   brew install --cask wechatwebdevtools
+    yes_or_no "omnidisksweeper" &&   brew install --cask omnidisksweeper
 }
 
 # 在脚本最后打印汇总信息，提示用户本次脚本执行后，一共做了哪些事情：成功安装了哪些软件
@@ -113,10 +154,12 @@ print_tips_after_install_finish() {
 # 我们推荐全部采用 zsh + oh-my-zsh：https://ohmyz.sh/
 
 if [ -f ~/.zshrc ]; then
-  (! command_exists "brew") && install_homebrew
   (! command_exists "node") && install_node_by_nvm
   (! command_exists "nrm") && install_global_npm_modules
+  (! command_exists "brew") && install_homebrew
+  (command_exists "brew") && install_with_homebrew
   print_tips_after_install_finish
+  echo '更多推荐软件，请参见：https://github.com/ly525/blog/issues/59'
 else
   # install_oh_my_zsh
   echo '~/.zshrc not exist, please use zsh and oh-my-zsh: https://ohmyz.sh/'
